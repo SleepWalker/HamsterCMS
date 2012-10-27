d1eb2ad087af64f42fd790e870e640535bf32eac
Pststudio <mybox@udf.su>
2012-10-27 16:11:44 +0200

    Добавил иконку с ссылкой на наш репозиторий на главной странице админки

--- protected/modules/admin/views/admin/index.php
+++ protected/modules/admin/views/admin/index.php
@@ -5,6 +5,7 @@ $this->pageTitle = 'Админка';
 
 <br /><br /><br /><br /><br /><br />
 <div style="float:right;">
-<img src="<?php echo $this->adminAssetsUrl; ?>/images/gplv3-88x31.png" alt="GPLv3" />
-<img src="<?php echo $this->adminAssetsUrl; ?>/images/yii-powered.png" alt="Yii Powered" />
+<a href="https://github.com/Pststudio/HamsterCMS" target="_blank"><img src="<?php echo $this->adminAssetsUrl; ?>/images/icon_git.png" alt="Мы на github.com" /></a>
+<a href="/LICENSE" target="_blank"><img src="<?php echo $this->adminAssetsUrl; ?>/images/gplv3-88x31.png" alt="GPLv3" /></a>
+<a href="http://www.yiiframework.com" target="_blank"><img src="<?php echo $this->adminAssetsUrl; ?>/images/yii-powered.png" alt="Yii Powered" /></a>
 </div>
