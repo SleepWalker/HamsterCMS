0e8b4979b75904412db81d976e67d9d6031a753f
SleepWalker <mybox@udf.su>
2014-06-29 16:42:17 +0300

    Теперь цмс не боится ошибок E_NOTICE

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-error_reporting(E_ALL ^ E_NOTICE);
+error_reporting(E_ALL);
 ini_set('display_errors', YII_DEBUG*1);
 ini_set("track_errors", YII_DEBUG*1);
 ini_set("html_errors", YII_DEBUG*1);
