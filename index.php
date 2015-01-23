ca88a6b7c6bf5d15dc9d9798f68602ab8ca5580b
SleepWalker <mybox@udf.su>
2015-01-23 09:40:36 +0200

    replaced NLSClientScript, hoauth with composer versions. Adeed yii2-debug in place of weblog

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../Yii.Hamster.Source/yii/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
-
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 // specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
-error_reporting(E_ALL);
-ini_set('display_errors', YII_DEBUG*1);
-ini_set("track_errors", YII_DEBUG*1);
-ini_set("html_errors", YII_DEBUG*1);
+error_reporting(E_ALL); // ^ E_NOTICE);
+ini_set('display_errors', YII_DEBUG * 1);
 
-require_once($yii);
+require dirname(__FILE__) . '/protected/vendor/composer/vendor/autoload.php';
+
+$config = dirname(__FILE__) . '/protected/config/main.php';
+
 Yii::createWebApplication($config)->run();
