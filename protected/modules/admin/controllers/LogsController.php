9d3f9ca8a9a9f085b14dc284e6b5d86601f2e162
SleepWalker <mybox@udf.su>
2016-02-18 10:01:12 +0200

    Improved logs controller

--- protected/modules/admin/controllers/LogsController.php
+++ protected/modules/admin/controllers/LogsController.php
@@ -12,23 +12,25 @@ namespace admin\controllers;
 
 class LogsController extends \admin\components\HAdminController
 {
+    private $filtersForm;
+
     public function filters()
     {
-        return array(
+        return [
             'accessControl',
-        );
+        ];
     }
 
     public function accessRules()
     {
-        return array(
-            array('allow',
-                'roles' => array('admin'),
-            ),
-            array('deny', // deny all users
-                'users' => array('*'),
-            ),
-        );
+        return [
+            ['allow',
+                'roles' => ['admin'],
+            ],
+            ['deny', // deny all users
+                'users' => ['*'],
+            ],
+        ];
     }
 
     public function actionIndex()
@@ -40,33 +42,84 @@ class LogsController extends \admin\components\HAdminController
             unset($_GET['FiltersForm'][0]);
             $filtersForm->filters = $_GET['FiltersForm'];
         }
+        $this->filtersForm = $filtersForm;
 
-        $logString = file_get_contents(\Yii::getPathOfAlias('application.runtime.application') . '.log');
+        $runtimePath = \Yii::app()->runtimePath;
+        $availableLogs = $this->getRuntimeLogs($runtimePath);
+
+        $selectedLogFile = isset($_GET['log']) && in_array($_GET['log'], $availableLogs) ? $_GET['log'] : reset($availableLogs);
+        $logFilePath = $runtimePath . '/' . $selectedLogFile;
+
+        list($logData, $categories) = $this->parseLog($logFilePath);
+
+        $dataProvider = new \CArrayDataProvider($logData, [
+            'id' => 'log',
+            'keyField' => false,
+            'pagination' => [
+                'pageSize' => 20,
+            ],
+        ]);
+
+        $this->render('log', [
+            'dataProvider' => $dataProvider,
+            'filtersForm' => $filtersForm,
+            'categories' => $categories,
+            'availableLogs' => array_combine($availableLogs, $availableLogs),
+        ]);
+    }
+
+    /**
+     * Возвращает массив с данными лога для использования в CGridView
+     *
+     * @param string $logFilePath путь к файлу лога для парсинга
+     *
+     * @return $logData, $logCategories
+     */
+    private function parseLog($logFilePath)
+    {
+        if (!is_readable($logFilePath)) {
+            throw new \InvalidArgumentException("$logFilePath is not readable or does not exists");
+        }
+
+        $filtersForm = $this->filtersForm;
+
+        $logString = file_get_contents($logFilePath);
+
         // добавляем разделитель, по которому будем делить строку
         $logString = preg_replace('/^(\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2})/m', '--Separator--$0', $logString);
+
         // Добавляем еще один сепаратор, что бы отображалась и последняя запись в логе
         $logString .= '--Separator--';
         preg_match_all('/(\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}) \[([^\]]+)\] \[([^\]]+)\] (.*?)--Separator--/s', $logString, $matches, PREG_SET_ORDER);
         $matches = array_reverse($matches);
-        $filteredData = $filtersForm->filter($matches);
+        $logData = $filtersForm->filter($matches);
+
+        $logCategories = [];
         foreach ($matches as $row) {
-            $categories[$row[3]] = $row[3];
+            $logCategories[$row[3]] = $row[3];
         }
 
-        asort($categories);
+        asort($logCategories);
 
-        $dataProvider = new \CArrayDataProvider($filteredData, array(
-            'id' => 'log',
-            'keyField' => false,
-            'pagination' => array(
-                'pageSize' => 20,
-            ),
-        ));
+        return [
+            $logData,
+            $logCategories,
+        ];
+    }
 
-        $this->render('log', array(
-            'dataProvider' => $dataProvider,
-            'filtersForm' => $filtersForm,
-            'categories' => $categories,
-        ));
+    /**
+     * @param string $path путь к директории рантайма
+     *
+     * @return array Названия файлов логов в папке райнтайма $path
+     */
+    private function getRuntimeLogs($runtimePath)
+    {
+        $logPathes = glob($runtimePath . DIRECTORY_SEPARATOR . '*.log*');
+        $logFiles = [];
+        foreach ($logPathes as $logPath) {
+            array_push($logFiles, basename($logPath));
+        }
+
+        return $logFiles;
     }
 }
--- protected/modules/admin/views/admin/log.php
+++ protected/modules/admin/views/admin/log.php
@@ -2,62 +2,71 @@
 
 $this->pageTitle = 'Логи';
 
-$this->widget('zii.widgets.grid.CGridView', array(
-    'dataProvider'=>$dataProvider,
-    'filter'=>$filtersForm,
-    'columns'=>array(
-      array(
-        'name' => '1',
-        'header' => 'Дата',
-        'value' => 'Yii::app()->dateFormatter->formatDateTime($data[1])',
-        'filter' => $this->widget('zii.widgets.jui.CJuiDatePicker', array(
-              'model'=> $filtersForm, 
-              'attribute'=>'1', 
-              'language' => Yii::app()->language,
-              'defaultOptions' => array(  
-                'showOn' => 'focus', 
-                'showOtherMonths' => true,
-                'selectOtherMonths' => true,
-                'changeMonth' => true,
-                'changeYear' => true,
-                'showButtonPanel' => true,
-                'autoSize' => true,
-                'dateFormat' => "yy/mm/dd",
-              ),
-              'options' => array(
-                'dateFormat' => "yy/mm/dd",
-              ),
-            ), true),
-      ),
-      array(
-        'name' => '2',
-        'header' => 'Уровень',
-        'filter' => array(
-          'error' => 'error',
-          'warning' => 'warning',
-          'info' => 'info',
-          'trace' => 'trace',
-          'profile' => 'profile',
-        ),
-        'cssClassExpression' => '"level_" . $data[2]',
-      ),
-      array(
-        'name' => '3',
-        'header' => 'Категория',
-        'filter' => $categories,
-      ),
-      array(
-        'name' => '4',
-        'header' => 'Сообщение',
-        'value' => '"<pre style=\"height:100px;overflow-y:auto;\">".CHtml::encode(wordwrap($data[4]))."</pre>"',
-        'type' => 'raw',
-      ),
-    ),
-    'cssFile'=>false,
+$globalFilters = '<span class="filters">';
+$globalFilters .= CHtml::dropDownList(
+    'log',
+    isset($_GET['log']) ? $_GET['log'] : '',
+    $availableLogs,
+    ['style' => 'margin: 10px 0;']
+);
+$globalFilters .= '</span>';
+
+$this->widget('zii.widgets.grid.CGridView', [
+    'dataProvider' => $dataProvider,
+    'filter' => $filtersForm,
+    'summaryText' => $globalFilters . Yii::t('zii', 'Displaying {start}-{end} of {count} result(s).'),
+    'columns' => [
+        [
+            'name' => '1',
+            'header' => 'Дата',
+            'value' => 'Yii::app()->dateFormatter->formatDateTime($data[1])',
+            'filter' => $this->widget('zii.widgets.jui.CJuiDatePicker', [
+                'model' => $filtersForm,
+                'attribute' => '1',
+                'language' => Yii::app()->language,
+                'defaultOptions' => [
+                    'showOn' => 'focus',
+                    'showOtherMonths' => true,
+                    'selectOtherMonths' => true,
+                    'changeMonth' => true,
+                    'changeYear' => true,
+                    'showButtonPanel' => true,
+                    'autoSize' => true,
+                    'dateFormat' => "yy/mm/dd",
+                ],
+                'options' => [
+                    'dateFormat' => "yy/mm/dd",
+                ],
+            ], true),
+        ],
+        [
+            'name' => '2',
+            'header' => 'Уровень',
+            'filter' => [
+                'error' => 'error',
+                'warning' => 'warning',
+                'info' => 'info',
+                'trace' => 'trace',
+                'profile' => 'profile',
+            ],
+            'cssClassExpression' => '"level_" . $data[2]',
+        ],
+        [
+            'name' => '3',
+            'header' => 'Категория',
+            'filter' => $categories,
+        ],
+        [
+            'name' => '4',
+            'header' => 'Сообщение',
+            'value' => '"<pre style=\"height:100px;overflow-y:auto;\">".CHtml::encode(wordwrap($data[4]))."</pre>"',
+            'type' => 'raw',
+        ],
+    ],
+    'cssFile' => false,
     'ajaxUpdate' => false,
-    'pager'=>array(
-      'cssFile'=>false,
-      'header'=>false,
-    ),
-));
-?>
+    'pager' => [
+        'cssFile' => false,
+        'header' => false,
+    ],
+]);
